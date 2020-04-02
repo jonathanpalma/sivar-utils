4338f8aae57c29abc3cd157c252b23e451c9e3a5
Francisco Alejandro Paz Tejada <francisco.paz@telusinternational.com>
2020-04-02 14:30:11 -0600

    feat: add nit validator

--- src/isNIT.ts
+++ src/isNIT.ts
@@ -1,11 +1,48 @@
 export const nitRegExp = /(^\d{4})-(\d{6})-(\d{3})-(\d$)/;
 
+/**
+ * Verifies that given NIT format is valid
+ *
+ * @param  {string} str       A string representing NIT digits (with hyphen)
+ * @returns {boolean}         Validity of the given NIT
+ */
+
+function isHyphenIndex(i: Number) {
+  return i === 4 || i === 11;
+}
+
+function calculateValidatorDigitOldFormat(str: string) {
+  let sum = 0;
+  for (let i = 0; i <= 14; i++) {
+    if (!isHyphenIndex(i)) sum += (15 - i) * Number(str.charAt(i));
+    sum %= 11;
+  }
+  return sum;
+}
+
+function calculateValidatorDigit(str: string) {
+  let n = 1;
+  let sum = 0;
+  for (let i = 0; i <= 14; i++) {
+    if (!isHyphenIndex(i)) {
+      sum +=
+        Number(str.charAt(i)) * (3 + 6 * Math.floor(Math.abs((n + 4) / 6)) - n);
+      n += 1;
+    }
+  }
+  sum %= 11;
+  return sum > 1 ? 11 - sum : 0;
+}
+
 function isNIT(str: string): boolean {
   if (!nitRegExp.test(str)) return false;
-
-  // TODO: Validate NIT
-
-  return true;
+  let sum = 0;
+  if (Number(str.substring(12, 15)) <= 100) {
+    sum = calculateValidatorDigitOldFormat(str);
+  } else {
+    sum = calculateValidatorDigit(str);
+  }
+  return sum === Number(str.charAt(16));
 }
 
 export default isNIT;
