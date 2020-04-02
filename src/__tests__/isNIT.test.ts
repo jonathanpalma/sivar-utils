0c6e6fc58edaf6ee0df251e1c363304a864ee727
Francisco Alejandro Paz Tejada <francisco.paz@telusinternational.com>
2020-04-02 14:43:06 -0600

    feat: add test cases for NIT

--- src/__tests__/isNIT.test.ts
+++ src/__tests__/isNIT.test.ts
@@ -1,6 +1,12 @@
 import isNIT from '../isNIT';
 
-const cases: [string, boolean][] = [['', false]];
+const cases: [string, boolean][] = [
+  ['', false],
+  ['0614-324234-111-5', false],
+  ['0101-010101-101-1', false],
+  ['0614-110790-113-7', true],
+  ['0614-080803-111-4', true],
+];
 
 describe('isNIT', () => {
   test.each(cases)('given %s should return %s', (arg, expected) => {
