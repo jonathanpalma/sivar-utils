d981d5633e091c119d8db43d1f5bd89dd163caf6
Jonathan Palma <tanpalma04@gmail.com>
2020-04-05 14:44:51 -0600

    refactor: refactor nit verification

--- src/isNIT.ts
+++ src/isNIT.ts
@@ -2,27 +2,26 @@ import isMunicipalityCode from './isMunicipalityCode';
 
 export const nitRegExp = /(^\d{4})-(\d{6})-(\d{3})-(\d$)/;
 
-function checkDigitOldFormat(digits: string) {
+function calculateVerificationOldFormat(digits: string) {
   let sum = 0;
   for (let i = 0; i < digits.length; i++) {
-    sum += (digits.length + 1 - i) * Number(digits.charAt(i));
-    sum %= 11;
+    sum += Number(digits[i]) * (digits.length + 1 - i);
   }
+  sum %= 11;
   return sum;
 }
 
-function checkDigit(digits: string) {
+function calculateVerification(digits: string) {
   let sum = 0;
   for (let i = 0; i < digits.length; i++) {
     sum +=
-      Number(digits.charAt(i)) *
-      (3 + 6 * Math.floor(Math.abs((i + 5) / 6)) - i + 1);
+      Number(digits[i]) * (3 + 6 * Math.floor(Math.abs((i + 5) / 6)) - (i + 1));
   }
   sum %= 11;
   return sum > 1 ? 11 - sum : 0;
 }
 
-function setFullYear(year: string) {
+function getFullYear(year: string) {
   return Number(year) >=
     Number(new Date().getFullYear().toString().substr(-2)) + 1
     ? '19'.concat(year)
@@ -30,7 +29,7 @@ function setFullYear(year: string) {
 }
 
 function isDate(str: string): boolean {
-  const year = setFullYear(str.substring(4));
+  const year = getFullYear(str.substring(4));
   const month = str.substring(2, 4);
   const day = str.substring(0, 2);
   const date = new Date(`${year}/${month}/${day}`);
@@ -47,17 +46,14 @@ function isNIT(str: string): boolean {
   if (!nitRegExp.test(str)) return false;
 
   const [municipality, birthdate, correlative, verifier] = str.split('-');
-  const digits = municipality + birthdate + correlative + verifier;
+  if (!isMunicipalityCode(municipality) || !isDate(birthdate)) return false;
 
-  if (!isMunicipalityCode(municipality)) return false;
-  if (!isDate(birthdate)) return false;
-  let sum = 0;
-  if (Number(correlative) <= 100) {
-    sum = checkDigitOldFormat(digits);
-  } else {
-    sum = checkDigit(digits);
-  }
-  return sum === Number(verifier);
+  const digits = municipality + birthdate + correlative;
+  const sum = correlative.startsWith('0')
+    ? calculateVerificationOldFormat(digits)
+    : calculateVerification(digits);
+
+  return Number(verifier) === sum;
 }
 
 export default isNIT;
