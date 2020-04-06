71d0d2ca37d7af9d6fe02ad18beff382bfe9ab4c
Jonathan Palma <tanpalma04@gmail.com>
2020-04-05 23:15:00 -0600

    chore: move jest config to package.json

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  roots: ['<rootDir>/src'],
-  transform: {
-    '\\.(ts)?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/public/'],
-};
