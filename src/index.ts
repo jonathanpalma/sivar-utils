be0917a42097f2d389ed23a09c4106a8053309a1
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-06-25 16:18:14 -0600

    feat(telephones): verify phone numbers

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export { isDUI, isNIT } from './lib/documents';
 export { isMunicipalityCode } from './lib/municipalities';
+export {
+  isMobilePhoneNumber,
+  isResidentialPhoneNumber,
+  isPhoneNumber,
+} from './lib/telephones';
